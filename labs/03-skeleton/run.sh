60a41d39fc90469d06d6832291becc7634dc8bc9
Nicolas "Norswap" Laurent <norswap@gmail.com>
2014-10-17 10:35:37 +0200

    remove skeleton from the labs directory, it is already in the VM directory

--- labs/03-skeleton/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Join arguments in a string. But not quite like you'd expect... see next line.
-printf -v var "'%s', " "$@"
-
-# Remove trailing ", "
-var=${var%??}
-
-./gradlew run -PappArgs="[$var]"
